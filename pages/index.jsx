f54024b9e30b166b3b3ebe08fdbf9d3c8d9d5612
Charlie Duong <charlieduong94@gmail.com>
2020-12-19 15:54:32 -0500

    Update deps and make necessary tweaks to get things to work again

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,13 +14,13 @@ const Page = ({ index }) => {
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/static/favicon-32x32.png"
+          href="/public/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/static/favicon-16x16.png"
+          href="/public/favicon-16x16.png"
         />
       </Head>
       <div className="content">
--- tools/generateLinks.js
+++ tools/generateLinks.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const path = require('path')
 
 const rootDir = path.resolve(__dirname, '..')
-const staticDir = path.resolve(rootDir, 'static', 'mochi')
+const staticDir = path.resolve(rootDir, 'public', 'mochi')
 
 const files = fs.readdirSync(staticDir)
 
-const links = files.map(file => `/static/mochi/${file}`)
+const links = files.map((file) => `/public/mochi/${file}`)
 
 fs.writeFileSync('links.json', JSON.stringify(links, null, 2), 'utf8')
 
 console.log('Generated the following links:')
-links.map(link => console.log(`  - ${link}`))
+links.map((link) => console.log(`  - ${link}`))
