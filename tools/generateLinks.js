6f0dfeeb277e40a696b105286dbd87ba3ad4541f
Charlie Duong <charlieduong94@gmail.com>
2020-12-19 15:58:14 -0500

    Remove "public" prefix from links

--- tools/generateLinks.js
+++ tools/generateLinks.js
@@ -6,7 +6,7 @@ const staticDir = path.resolve(rootDir, 'public', 'mochi')
 
 const files = fs.readdirSync(staticDir)
 
-const links = files.map((file) => `/public/mochi/${file}`)
+const links = files.map((file) => `/mochi/${file}`)
 
 fs.writeFileSync('links.json', JSON.stringify(links, null, 2), 'utf8')
 
