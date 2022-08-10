811713ec4bd3b3fb3ba6c21881ec655fdfc3ddc9
Charlie Duong <charlieduong94@gmail.com>
2022-08-09 22:19:44 -0400

    Make insta link a big button

--- components/SwipeableCards.jsx
+++ components/SwipeableCards.jsx
@@ -7,13 +7,19 @@ export const SwipeableCards = ({ images }) => {
   return (
     <div className="flex justify-center align-center grow mt-10 w-full text-slate-100">
       <div className="absolute flex flex-col justify-center align-center text-xs">
-        <a href="https://instagram.com/mocheroni">
-          <div className="flex flex-row justify-center align-center text-center">
+        <a
+          href="https://instagram.com/mocheroni"
+          className="flex justify-center align-center text-purple-500"
+        >
+          <button
+            type="button"
+            className="bg-white rounded flex flex-row justify-center align-center text-center p-1"
+          >
             <InstagramIcon className="m-0" />{' '}
             <p className="text-sm pl-1">mocheroni</p>
-          </div>
+          </button>
         </a>
-        <p className="mt-5">Refresh to start over!</p>
+        <p className="mt-5">Check his insta for more pics</p>
       </div>
 
       <div className="w-64">
