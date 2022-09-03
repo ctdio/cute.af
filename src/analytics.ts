976afb3ff6a4b2682f45d94c057f6fabc052f342
Charlie Duong <charlieduong94@gmail.com>
2022-09-03 15:44:29 -0400

    Tweak mixpanel env var

--- src/analytics.ts
+++ src/analytics.ts
@@ -4,7 +4,7 @@ const SWIPE_EVENT_NAME = 'Mochi Swipe'
 
 // Enabling the debug mode flag is useful during implementation,
 // but it's recommended you remove it for production
-mixpanel.init(import.meta.env.PUBLIC_MIXPANEL_PROJECT_ID, {
+mixpanel.init(import.meta.env.PUBLIC_MIXPANEL_PROJECT_TOKEN, {
   debug: import.meta.env.DEV,
 })
 mixpanel.track('Visited page')
