15fa38ae23279339461d1ea87b4b6da5f51038e2
Charlie Duong <charlieduong94@gmail.com>
2022-09-25 14:06:04 -0400

    Add playwright, fix formatting

--- __checks__/page-load.check.js
+++ __checks__/page-load.check.js
@@ -1,17 +1,11 @@
-/**
- * This is a Playwright script Checkly generated for you based on your Vercel project.
- * To learn more about Browser checks and Playwright visit: https://www.checklyhq.com/docs/browser-checks
- */
-
-// Create a Chromium browser
 const { chromium } = require('playwright')
 
-// Checkly supports top level await, but we wrap your code in an async function so you can run it locally too.
-async function run () {
+async function run() {
   const browser = await chromium.launch()
   const page = await browser.newPage()
 
-  // If available, we set the target URL to a preview deployment URL provided by the ENVIRONMENT_URL created by Vercel.
+  // If available, we set the target URL to a preview deployment
+  // URL provided by the ENVIRONMENT_URL created by Vercel.
   // Otherwise, we use the Production URL.
   const targetUrl = process.env.ENVIRONMENT_URL || 'https://cute.af'
 
@@ -26,7 +20,8 @@ async function run () {
   // We snap a screenshot.
   await page.screenshot({ path: 'screenshot.jpg' })
 
-  // We close the page and browser. This is needed for collecting accurate web vitals.
+  // We close the page and browser.
+  // This is needed for collecting accurate web vitals.
   await page.close()
   await browser.close()
 }
